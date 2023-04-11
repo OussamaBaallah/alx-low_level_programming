3ebd82fd80b77b260115ed7455911c078f23466e
OussamaBaallah <oussamabaalla10@gmail.com>
2023-04-11 04:00:49 -0700

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,84 +1,45 @@
 #include "main.h"
-		
 #include <stdlib.h>
-		
+
 /**
-		
- * str_concat - get ends of input and add together for size
-		
- * @s1: input one to concat
-		
- * @s2: input two to concat
-		
- * Return: concat of s1 and s2
-		
+ * *str_concat - Entry point
+ * Description: Concatenates two strings
+ * @s1: Character
+ * @s2: Character
+ * Return: Char
  */
-		
+
 char *str_concat(char *s1, char *s2)
-		
 {
-		
-	char *conct;
-		
-	int i, ci;
-		
+	int len = 0, i, j = 0;
+	char *ans;
 
-		
 	if (s1 == NULL)
-		
+	{
 		s1 = "";
-		
+	}
 	if (s2 == NULL)
-		
+	{
 		s2 = "";
-		
-
-		
-		i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
-		i++;
-		
-	while (s2[ci] != '\0')
-		
-		ci++;
-		
-	conct = malloc(sizeof(char) * (i + ci + 1));
-		
-
-		
-	if (conct == NULL)
-		
-		return (NULL);
-		
-	i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
-	{
-		
-		conct[i] = s1[i];
-		
-		i++;
-		
 	}
-		
-
-		
-	while (s2[ci] != '\0')
-		
+	for (i = 0; s1[i] || s2[i]; i++)
 	{
-		
-		conct[i] = s2[ci];
-		
-		i++, ci++;
-		
+		len++;
 	}
-		
-	conct[i] = '\0';
-		
-	return (conct);
-		
+
+	ans = malloc((len) * sizeof(char));
+	/* Allocates memory for the ans string */
+	if (ans == NULL)
+	{
+		return (NULL); /* Allocation failed */
+	}
+	for (i = 0; s1[i]; i++)
+	{
+		ans[j++] = s1[i];
+	} /* Copies contents of s1 to the ans string */
+	for (i = 0; s2[i]; i++)
+	{
+		ans[j++] = s2[i];
+	} /* Copies contents of s2 to the ans string */
+	return (ans);
 }
-
